bcb7405dbf698e8a9aee152b82f0ea6a1791990d
Rafael Tosta <rafa.ecomp@gmail.com>
2023-10-11 17:09:25 -0300

    :sparkles: feat: Main page

--- src/App.tsx
+++ src/App.tsx
@@ -1,24 +1,16 @@
-import reactLogo from './assets/react.svg'
-import viteLogo from '/vite.svg'
+import Navbar from './components/navbar'
+import Home from './partials/home'
+import Donate from './partials/donate'
+
 
 function App() {
 
   return (
-    <div className='w-screen h-screen flex flex-col justify-center items-center gap-4'>
-      <div className='flex gap-6'>
-        <a href="https://vitejs.dev" target="_blank">
-          <img src={viteLogo} className="w-20 h-20" alt="Vite logo" />
-        </a>
-        <a href="https://react.dev" target="_blank">
-          <img src={reactLogo} className="w-20 h-20" alt="React logo" />
-        </a>
-      </div>
-
-      <h1 className="text-3xl font-bold">
-        Vite + React
-      </h1>
-    </div>
-
+    <main>
+      <Navbar />
+      <Home />
+      <Donate />
+    </main>
   )
 }
 
