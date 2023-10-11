62a55e3d4a5d2607b02122390761a8b929dde082
Rafael Tosta <rafa.ecomp@gmail.com>
2023-10-11 17:05:41 -0300

    :construction: Donate page component

---
+++ src/partials/donate.tsx
@@ -0,0 +1,10 @@
+function Donate() {
+    return (
+        <div id='donate'
+            className="bg-white min-h-screen flex flex-col items-center justify-center p-20">
+            <p>Doações em construção....</p>
+        </div>
+    );
+}
+
+export default Donate;
